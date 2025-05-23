333bdcd7da26e05a336ab519399938eb5e2e4879
David Bernet <david@bernet.it>
2025-05-23 17:33:38 +0200

    Refactored und dokumentiert
    Paar kommentare hinzugefügt, damit man versteht was so passiert

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
+/*
+
+  Eine eigene Anwendung entwickeln in 4 Schritten:
+
+  1. Eine Methode nach den Beispielen in example_apps.ino anlegen. Entweder dort, oder in einer neuen .ino-Datei
+    - Die Anwendung muss eine blockierende Funktion sein. (z.B. mit delay()-Funktion, oder while-Schleife)
+    - Mit einem return aus der Methode, wird die Anwendung beendet
+  2. Vorwärtsdeklaration in dieser Datei (siehe unten) einfügen, damit der Compiler schon von von Anfang an die Signatur der Methode kennt.
+  3. Einen Menüeintrag mit Titel und Methodenname im menu[]-Array (siehe unten) eintragen.
+  4. Das Projekt kompilieren!
+
+*/
+
 #include "base/display.h"
 #include "base/led.h"
 #include "base/button.h"
 #include "base/pins.h"
 #include <WiFi.h>
 
-
+// Vorwärts-Deklaration der Anwendungen
 void appInfo();
 void appSerialDemo();
 void appScanWiFi();
@@ -12,6 +25,7 @@ void appSplashImage();
 void appBlink();
 void appTasterTest();
 
+// Deklaration des Haupt-Menüs
 void drawMenu();
 void handleMenuInput();
 
@@ -40,30 +54,12 @@ void setup() {
     led::init();
     display::init();
 
+    // splash-Image am Anfang
     display::clearDisplay();
-
     display::drawSplashImage();
     display::display();
-    delay(4000);
 
-    // Six example options
-    // const char* options[] = {
-    //     "Lionbbb",
-    //     "Tiger",
-    //     "Bear",
-    //     "Elephant",
-    //     "Giraffe",
-    //     "Zebra"
-    // };
-    // int todo = display::makeMenu("Button Test", options, 6);
-    // display::clearDisplay();
-
-    // // Print Result
-    // display::print("Selected Option: ");
-    // display::println(String(todo));
-    // display::println(options[todo]);
-    // display::display();
-    // delay(3000);
+    delay(4000); // <- hier ändern, wenn das Splash Image zu lang da is
 }
 
 // Menü-Zustand
@@ -77,39 +73,6 @@ void loop() {
         drawMenu();
         delay(50);
     }
-
-    // String msg_l = "";
-    // String msg_r = "";
-
-
-    // if (button::down(BTN1)) { led::set_l(255,0,0); msg_l = "BTN1: Left Red"; }
-    // else if (button::down(BTN2)) { led::set_l(0,255,0); msg_l = "BTN2: Left Green"; }
-    // else if (button::down(BTN3)) { led::set_l(0,0,255); msg_l = "BTN3: Left Blue"; }
-    // else
-    // {
-    //     led::set_l(0,0,0);
-    //     msg_l = "--";
-    // }
-    // if (button::down(BTN4)) { led::set_r(255,0,0); msg_r = "BTN4: Right Red"; }
-    // else if (button::down(BTN5)) { led::set_r(0,255,0); msg_r = "BTN5: Right Green"; }
-    // else if (button::down(BTN6)) { led::set_r(0,0,255); msg_r = "BTN6: Right Blue"; }
-    // else
-    // {
-    //     led::set_r(0,0,0);
-    //     msg_r = "--";
-    // }
-
-
-    // // Update display::display
-    // display::clearDisplay();
-    // display::println("Button Test");
-    // display::println(msg_l);
-    // display::println(msg_r);
-    // display::println("Press any button");
-    // display::println("to change color");
-    // display::display();
-
-    // delay(100);
 }
 
 // Eingabelogik
@@ -136,33 +99,32 @@ void handleMenuInput() {
 
 // Menüanzeige
 void drawMenu() {
-  display::clearDisplay();
+    display::clearDisplay();
 
-  int visibleLines = 3;
-  int start = selected - (selected % visibleLines); // Startindex des sichtbaren Blocks
+    int visibleLines = 3;
+    int start = selected - (selected % visibleLines); // Startindex des sichtbaren Blocks
 
-  for (int i = 0; i < visibleLines; i++) {
-    int index = start + i;
-    if (index >= menuSize) break;
+    for (int i = 0; i < visibleLines; i++) {
+        int index = start + i;
+        if (index >= menuSize) break;
 
-    if (index == selected)
-      display::setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Farben invertieren
-    else
-      display::setTextColor(SSD1306_WHITE, SSD1306_BLACK);
+        if (index == selected)
+            display::setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Farben invertieren
+        else
+            display::setTextColor(SSD1306_WHITE, SSD1306_BLACK);
 
-    // display.setCursor(0, i * 10);
-    display::println(menu[index].title);
-  }
+        // display.setCursor(0, i * 10);
+        display::println(menu[index].title);
+    }
 
-  // Eintrag-Nummer unten rechts anzeigen
-    // display::setTextColor(SSD1306_WHITE, SSD1306_BLACK);
-    // display::setCursor(108, 22);
-    // display::print(selected+1);
-    // display::print("/");
-    // display::print(menuSize);
-
-  display::display();
+    display::display();
 }
+
+
+/*
+    Ab hier werden die Anwendungen definiert!
+    Trage neue Anwendungen oben als Menü-Eintrag ein!
+*/
 
 void appSplashImage() {
     display::clearDisplay();
@@ -171,15 +133,64 @@ void appSplashImage() {
     delay(1000);
 }
 
+// Text auf dem Display anzeigen:
 void appInfo() {
     display::println("Chaos.JETZT! #9");
     display::println("26.05.2025 in Leipzig");
     display::println("");
     display::println("ESP-C3 - v1.0");
     display::display();
+    // wird die Vereinsgründung klappen?
     delay(5000);
 }
 
+// LEDs Blinken lassen:
+void appBlink() {
+    display::println("Blinking LED...");
+    display::display();
+
+    for (int i = 0; i < 5; i++) {
+        led::set_r(100, 0, 0);
+        delay(250);
+        led::set_r(0, 100, 0);
+        delay(250);
+        led::set_r(0, 0, 100);
+        delay(250);
+        led::set_r(0, 0, 0);
+        
+        led::set_l(100, 0, 0);
+        delay(250);
+        led::set_l(0, 100, 0);
+        delay(250);
+        led::set_l(0, 0, 100);
+        delay(250);
+        led::set_l(0, 0, 0);
+    }
+}
+
+// Eingaben der Taster erfassen:
+void appTasterTest() {
+  while(true) {
+    display::clearDisplay();
+    display::setCursor(0, 0);
+
+    display::print(button::down(BTN1) ? "X " : "O ");
+    display::print(button::down(BTN2) ? "X " : "O ");
+    display::print(button::down(BTN3) ? "X " : "O ");
+    display::print(button::down(BTN4) ? "X " : "O ");
+    display::print(button::down(BTN5) ? "X " : "O ");
+    display::print(button::down(BTN6) ? "X " : "O ");
+
+    if (button::down(BTN1) && button::down(BTN2)) {
+      return;
+    }
+
+    display::display();
+    delay(100);
+  }
+}
+
+// Diese Anwendung macht noch Probleme. Sie soll mit der seriellen Konsole kommunizieren.
 void appSerialDemo() {
   delay(200);
 
@@ -217,6 +228,7 @@ void appSerialDemo() {
   } 
 }
 
+// Nach W-Lan-Netzen suchen und die SSIDs über die serielle Konsole ausgeben
 void appScanWiFi() {
   display::println("Starte WiFi-Scan...");
   display::display();
@@ -244,48 +256,4 @@ void appScanWiFi() {
   display::println("siehe Serial Monitor!");
   display::display();
   delay(5000);
-}
-
-void appBlink() {
-    display::println("Blinking LED...");
-    display::display();
-
-    for (int i = 0; i < 5; i++) {
-        led::set_r(100, 0, 0);
-        delay(250);
-        led::set_r(0, 100, 0);
-        delay(250);
-        led::set_r(0, 0, 100);
-        delay(250);
-        led::set_r(0, 0, 0);
-        
-        led::set_l(100, 0, 0);
-        delay(250);
-        led::set_l(0, 100, 0);
-        delay(250);
-        led::set_l(0, 0, 100);
-        delay(250);
-        led::set_l(0, 0, 0);
-    }
-}
-
-void appTasterTest() {
-  while(true) {
-    display::clearDisplay();
-    display::setCursor(0, 0);
-
-    display::print(button::down(BTN1) ? "X " : "O ");
-    display::print(button::down(BTN2) ? "X " : "O ");
-    display::print(button::down(BTN3) ? "X " : "O ");
-    display::print(button::down(BTN4) ? "X " : "O ");
-    display::print(button::down(BTN5) ? "X " : "O ");
-    display::print(button::down(BTN6) ? "X " : "O ");
-
-    if (button::down(BTN1) && button::down(BTN2)) {
-      return;
-    }
-
-    display::display();
-    delay(100);
-  }
 }
